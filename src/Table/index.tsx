5f64c2bac39c1b10cfd5fc29cc0ccb8bac550e12
nodirbeksulaymonov <s.nodir9494@gmail.com>
2021-02-12 00:15:25 +0300

    add Loading for Table

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -11,61 +11,68 @@ import useGetUsers from './HTTPrequst';
 
 const TableComponent: FC = () => {
     const [isLoading, users] = useGetUsers('https://5ede19ffe36dd000166c7f2a.mockapi.io/v1/user/');
-    const [todo, setTodo] = useState(null);
-    console.log(isLoading)
+    const [todo, setTodo] = useState();
+
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const buttonElement = event.target as HTMLButtonElement;
+        let id = buttonElement.dataset.id;
         // @ts-ignore
-        let id = event.target.dataset.id;
         setTodo(id)
     }
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Location</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Phone number</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.map((row: any) => (
-                        <React.Fragment key={row.id}>
+        <>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
                             <TableRow>
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                    <button data-id={row.id} onClick={handleClick} className="addColumn">+</button>
-                                </TableCell>
-                                <TableCell>{row.location}</TableCell>
-                                <TableCell>{row.email}</TableCell>
-                                <TableCell>{row.phone}</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Location</TableCell>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Phone number</TableCell>
                             </TableRow>
-                            {row.id === todo && (
-                                <TableRow>
-                                    <TableCell>
-                                        <input className="inputRoot"
-                                               placeholder="Name"/>
-                                    </TableCell>
-                                    <TableCell>
-                                        <input className="inputRoot"
-                                               placeholder="Location"/>
-                                    </TableCell>
-                                    <TableCell>
-                                        <input className="inputRoot"
-                                               placeholder="email@rmail.com"/>
-                                    </TableCell>
-                                    <TableCell>
-                                        <input className="inputRoot"
-                                               placeholder="Phone number"/>
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {users.map((row: any) => (
+                                <React.Fragment key={row.id}>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                            <button data-id={row.id} onClick={handleClick} className="addColumn">+</button>
+                                        </TableCell>
+                                        <TableCell>{row.location}</TableCell>
+                                        <TableCell>{row.email}</TableCell>
+                                        <TableCell>{row.phone}</TableCell>
+                                    </TableRow>
+                                    {row.id === todo && (
+                                        <TableRow>
+                                            <TableCell>
+                                                <input className="inputRoot"
+                                                       placeholder="Name"/>
+                                            </TableCell>
+                                            <TableCell>
+                                                <input className="inputRoot"
+                                                       placeholder="Location"/>
+                                            </TableCell>
+                                            <TableCell>
+                                                <input className="inputRoot"
+                                                       placeholder="email@rmail.com"/>
+                                            </TableCell>
+                                            <TableCell>
+                                                <input className="inputRoot"
+                                                       placeholder="Phone number"/>
+                                            </TableCell>
+                                        </TableRow>
+                                    )}
+                                </React.Fragment>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )}
+        </>
     );
 }
 
